fb6140f8bd1f60bdef0a4993220ed6a22e700d4c
aditehh <aditya.aryal888@gmail.com>
2026-02-01 13:37:51 +0545

    this

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,8 +1,34 @@
 import { syncUser } from "@/lib/domain";
 import prisma from "@/lib/db";
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
+    try {
+        const body = await request.json();
 
-    
+        const { email, name, image } = body;
+
+        if (!email || !name) {
+            return NextResponse.json(
+                { error: "email and name are required" },
+                { status: 400 }
+            )
+        }
+
+        const user = await syncUser(email, name, image)
+
+        return NextResponse.json({ user }, { status: 200 })
+
+    } catch (error) {
+
+        console.error("Login error:", error)
+        return NextResponse.json({
+            error: "internal server error"
+        },
+            {
+                status: 500
+            })
+
+    }
 
 }
