53a925bfe1edcdbf55c309dc218a01b160f34099
aditehh <aditya.aryal888@gmail.com>
2026-02-01 12:26:53 +0545

    better auth

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,6 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
 import { PrismaPg } from '@prisma/adapter-pg'
 
-
-
-
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL
 })
