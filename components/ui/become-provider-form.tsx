21d0a97b7bf0f3f35b830f04d1769632d1adc26b
aditehh <aditya.aryal888@gmail.com>
2026-02-02 21:03:58 +0545

    this

--- components/ui/become-provider-form.tsx
+++ components/ui/become-provider-form.tsx
@@ -11,34 +11,61 @@ import {
   ComboboxItem,
   ComboboxList,
 } from "@/components/ui/combobox"
+import { useState } from 'react'
 
 const locations = ["pokhara", "Itahari", "Dharan", "Janakpur", "Hetauda", "Butwal", "Nepalgunj", "Ghorahi", "Tulsipur", "Kalaiya", "Jitpur", "Simara", "Kathmandu", "Lalitpur", "Bharatpur", "Birgunj", "Biratnagar"]
 
 
 
 export default function BecomeProviderForm() {
-  return (
-    <form action={becomeProviderAction}>
-      {/* <Input name="location" placeholder='location' /> */}
-      <Combobox items={locations}>
-        <ComboboxInput name='location' placeholder="Select a framework" />
-        <ComboboxContent>
-          <ComboboxEmpty>No items found.</ComboboxEmpty>
-          <ComboboxList>
-            {(item) => (
-              <ComboboxItem key={item} value={item}>
-                {item}
-              </ComboboxItem>
-            )}
-          </ComboboxList>
-        </ComboboxContent>
-      </Combobox>
 
-      <Input name='bio' placeholder='bio' />
-      <Input name='experience' placeholder='experience' type='number' />
-      <Button type='submit'>
-        Become a provider
-      </Button>
-    </form>
+  const [location, setLocation] = useState("")
+  const [bio, setBio] = useState("")
+  const [experience, setExperience] = useState("")
+
+  const isdisabled = !location || !bio || !experience
+  return (
+
+
+    <form action={becomeProviderAction}>
+  {/* hidden real input */}
+  <input type="hidden" name="location" value={location} />
+
+  <Combobox items={locations}>
+    <ComboboxInput
+      value={location}
+      onChange={(e) => setLocation(e.target.value)}
+      placeholder="Select a location"
+    />
+
+    <ComboboxContent>
+      <ComboboxEmpty>No items found.</ComboboxEmpty>
+      <ComboboxList>
+        {(item) => (
+          <ComboboxItem
+            key={item}
+            value={item}
+            onSelect={() => setLocation(item)}
+          >
+            {item}
+          </ComboboxItem>
+        )}
+      </ComboboxList>
+    </ComboboxContent>
+  </Combobox>
+
+  <Input name="bio" value={bio} onChange={(e) => setBio(e.target.value)} />
+  <Input
+    name="experience"
+    value={experience}
+    onChange={(e) => setExperience(e.target.value)}
+    type="number"
+  />
+
+  <Button type="submit" disabled={isdisabled}>
+    Become a provider
+  </Button>
+</form>
+
   )
 }
