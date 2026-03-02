1238bc7069bdb4e26eb14bc19473edb103edfde8
aditehh <aditya.aryal888@gmail.com>
2026-03-02 23:53:59 +0545

    nela

--- app/notification/page.tsx
+++ app/notification/page.tsx
@@ -12,11 +12,21 @@ export default async function NotificationPage() {
         <div>
             {notifications.length === 0 ? (
                 <div>No Notifications...</div>
-            ): (
+            ) : (
                 <div>
-                    {notifications.map(notification) => (
-                        <div key={notification.id}></div>
-                    )}
+                    {notifications.map((notification) => (
+                        <div key={notification.id}>
+                            {notification.message}
+                            <span>
+                                {notification.type}
+                            </span>
+                            {new Date(notification.createdAt).toLocaleDateString("en-US", {
+                                year: "numeric",
+                                month: "long",
+                                day: "numeric",
+                            })}
+                        </div>
+                    ))}
                 </div>
             )}
 
