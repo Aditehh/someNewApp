7f57ccc149bf036920977ecc79aa864d66606974
aditehh <aditya.aryal888@gmail.com>
2026-03-06 22:39:15 +0545

    halonema

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -3,6 +3,7 @@
 import CreateBookingsButton from "@/components/ui/create-bookings-button";
 import Navbar from "@/components/ui/navbar";
 import { getPublishedServices } from "@/lib/domain";
+import { Link } from "lucide-react";
 
 export default async function MarketplacePage() {
 
@@ -32,41 +33,43 @@ export default async function MarketplacePage() {
                     ) : (
                         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
                             {services.map((service) => (
-                                <div
-                                    key={service.id}
-                                    className="bg-white rounded-xl shadow-sm border p-6 hover:shadow-md transition flex flex-col justify-between"
-                                >
+                                <Link href="/">
+                                    <div
+                                        key={service.id}
+                                        className="bg-white rounded-xl shadow-sm border p-6 hover:shadow-md transition flex flex-col justify-between"
+                                    >
 
-                                    <div>
-                                        <h3 className="text-lg font-semibold text-slate-800">
-                                            {service.title}
-                                        </h3>
+                                        <div>
+                                            <h3 className="text-lg font-semibold text-slate-800">
+                                                {service.title}
+                                            </h3>
 
-                                        <p className="text-sm text-slate-500 mt-1">
-                                            {service.category?.name}
+                                            <p className="text-sm text-slate-500 mt-1">
+                                                {service.category?.name}
+                                            </p>
+                                        </div>
+
+                                        <p className="text-slate-600 text-sm mt-4 line-clamp-2">
+                                            {service.description}
                                         </p>
+
+                                        <div className="mt-4 text-sm text-slate-500 space-y-1">
+                                            <div>
+                                                <span className="font-medium text-slate-700">
+                                                    Rs. {service.price}
+                                                </span>
+                                            </div>
+                                            <div>
+                                                {service.duration} mins
+                                            </div>
+                                            <div>
+                                                Provider: {service.provider.user.name}
+                                            </div>
+                                        </div>
+
+                                        {/* <CreateBookingsButton serviceId={service.id} /> */}
                                     </div>
-
-                                    <p className="text-slate-600 text-sm mt-4 line-clamp-2">
-                                        {service.description}
-                                    </p>
-
-                                    <div className="mt-4 text-sm text-slate-500 space-y-1">
-                                        <div>
-                                            <span className="font-medium text-slate-700">
-                                                Rs. {service.price}
-                                            </span>
-                                        </div>
-                                        <div>
-                                            {service.duration} mins
-                                        </div>
-                                        <div>
-                                            Provider: {service.provider.user.name}
-                                        </div>
-                                    </div>
-
-                                    <CreateBookingsButton serviceId={service.id} />
-                                </div>
+                                </Link>
                             ))}
                         </div>
                     )}
