ed05a2320c2aab7a7b2bcaf2591262fd0c206b9d
aditehh <aditya.aryal888@gmail.com>
2026-03-08 22:55:29 +0545

    leaner

--- app/services/[serviceId]/page.tsx
+++ app/services/[serviceId]/page.tsx
@@ -91,7 +91,7 @@ export default async function ServicePage({
                             <CreateBookingsButton serviceId={service.id} />
 
                         </div>
-                        <EmojiReview bookingId={} />
+                        <EmojiReview bookingId={booking.id} />
                     </div>
 
 
